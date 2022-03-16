5a01fea347bdf3660a18bc1daacbed5eabe58ee3
crinkj <jcrinkey89120@gmail.com>
2022-03-17 02:10:47 +0900

    test commit

--- src/main/kotlin/jpa/test/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/jpa/test/advice/GlobalControllerAdvice.kt
@@ -1,8 +1,10 @@
 package jpa.test.advice
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 // RestController에서 발생하는 exception은 다 여기로 통하게 설정하는 annotation
 @RestControllerAdvice
 class GlobalControllerAdvice {
+    @GetMapping
 }
