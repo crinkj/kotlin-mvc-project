08a000a5bf29621ca4a00c3c7ba14455e818ac68
crinkj <jcrinkey89120@gmail.com>
2022-03-18 23:34:13 +0900

    Add: 공통 exceptionHandler 완료

--- src/main/kotlin/jpa/test/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/jpa/test/advice/GlobalControllerAdvice.kt
@@ -1,10 +1,22 @@
 package jpa.test.advice
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 // RestController에서 발생하는 exception은 다 여기로 통하게 설정하는 annotation
 @RestControllerAdvice
 class GlobalControllerAdvice {
-    @GetMapping
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    fun exception(e : RuntimeException): String{
+        return "Server Error"
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String> {    // 200 OK
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+    }
 }
